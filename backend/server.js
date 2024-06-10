6b25ba543db53f2c829174c5248670ecc539dd57
CodinJack <IIT2022005@iiita.ac.in>
2024-06-10 12:46:04 +0300

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-const express = require("express")
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./database/db');
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
