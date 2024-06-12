028e474e1099677c48a268fb2021b43a0538e6a7
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-06-12 09:21:49 +0400

    [prisma] Added model for the transaction table

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -8,3 +8,23 @@ CREATE TABLE `users` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `expenses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `amount` DECIMAL(10, 2) NOT NULL,
+    `type` VARCHAR(50) NULL DEFAULT 'expense',
+    `date` DATE NOT NULL,
+    `category` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(20) NOT NULL,
+    `createdAt` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updatedAt` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    INDEX `user_id`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `expenses` ADD CONSTRAINT `expenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
