e6d4c3b3b02dae0a649931f84e0026215a1d10de
CodinJack <IIT2022005@iiita.ac.in>
2024-06-12 10:04:55 +0300

    changes in prisma

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,30 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const { db } = require('./database/db');
-const {readdirSync} = require('fs')
-const app = express()
+const { readdirSync } = require('fs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/', require('./routes/' + route)))
+readdirSync('./routes').map((route) => app.use('/api/', require('./routes/' + route)));
 
 const server = () => {
-    db()
     app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+        console.log('Listening on port:', PORT);
+    });
+};
 
-server()
+server();
+
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+});
