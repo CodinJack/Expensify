0104dc998221d1e7a9497f8676a80e0e5d80d643
Samuel E George <142198315+titanick007@users.noreply.github.com>
2024-06-12 10:48:01 +0530

    Inserted expenses table

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -8,3 +8,16 @@ CREATE TABLE `users` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE `expenses` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `user_id` INT,
+    `title` VARCHAR(50) NOT NULL,
+    `amount` DECIMAL(10, 2) NOT NULL,
+    `type` VARCHAR(50) DEFAULT 'expense',
+    `date` DATE NOT NULL,
+    `category` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(20) NOT NULL,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
