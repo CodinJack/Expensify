06c2b5a98813d98fcc15e363777e29a43b7d64af
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-06-13 14:28:55 +0400

    implemented basic authentication. moved prisma folder to backend folder

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const path = require("path");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
 const router = express.Router();
+const authController = require("../controllers/auth");
 
-router.post("/login", createUsers);
+router.post(
+    "/log-in",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/failure",
+    })
+);
 
+router.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+
 const { readdirSync } = require("fs");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
@@ -12,11 +15,26 @@ const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(express.json());
 app.use(cors());
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 //routes
 readdirSync("./routes").map((route) =>
     app.use("/api/", require("./routes/" + route))
 );
+
+app.set("views", "view");
+app.set("view engine", "ejs");
+app.get("/log-in", (req, res) => res.render("log-in"));
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user });
+});
+app.get("/failure", (req, res) => res.render("failure"));
 
 const server = () => {
     app.listen(PORT, () => {
